21d39c4d07e12a9529104f2527d4d6433cfd2428
Emmanuel Kala <ekala@pegasus.(none)>
2010-12-07 17:17:40 +0300

    Added hook for generating the category tag cloud

---
+++ hooks/categorycloud.php
@@ -0,0 +1,127 @@
+<?php defined('SYSPATH') or die('No direct script access'); 
+/**
+ * Category Cloud Hook - Load all events
+ *
+ * PHP Version 5
+ * LICENSE: This source file is subject to LGPL license that is available
+ * through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ * @author      Ushahidi Team <team(at)ushahidi.com>
+ * @package     Ushahidi - http://source.ushahididev.com
+ * @copyright   Ushahidi - http://www.ushahidi.com
+ * @license     http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
+ */
+ 
+ class categorycloud {
+     private $max_font_size = 36;   // Maximum font size
+     private $min_font_size = 8;   // Minimum font size
+     
+     /**
+      * Registers the main event add method
+      */
+      public function __construct()
+      {
+          // Hook into routing
+          Event::add('system.pre_controller', array($this, 'add'));
+      }
+      
+      /**
+       * Adds all the events to the main Ushahidi application
+       */
+      public function add()
+      {
+          if (Router::$controller == 'main')
+          {
+              Event::add('ushahidi_action.main_sidebar', array($this, '_generate_cloud'));
+          }
+      }
+      
+      /**
+       * Generates the category tag cloud
+       */
+      public function _generate_cloud()
+      {
+          // SQL query to generate the categories
+          $query = "SELECT c.id, c.category_title, c.category_color, SUM(c1.incident_count) tag_frequency
+                    FROM
+                      (SELECT ic.category_id, ifnull(c.parent_id,0) parent_id, COUNT(ic.incident_id) incident_count
+                       FROM incident_category ic
+                       LEFT JOIN category c ON (ic.category_id = c.id)
+                       LEFT JOIN incident i ON (ic.incident_id = i.id)
+                       WHERE i.incident_active = 1
+                       GROUP BY ic.category_id
+                      ) c1
+                    LEFT JOIN category c ON (c1.parent_id = c.id OR (c.parent_id = 0 AND c1.category_id = c.id))
+                    GROUP BY c.id
+                    ORDER BY 2 ASC";
+        
+          // Instantiate the database
+          $db = new Database();
+          
+          // Execute the query
+          $category_items = $db->query($query);
+          
+          $min_frequency = -1; // Minimum tag frequency
+          $max_frequency = -1; // Maximum tag frequency
+          
+          // Determine the maximum and minimum tag frequencies
+          foreach ($category_items as $item)
+          {
+              // Get the current tag frequency
+              $frequency = (int)$item->tag_frequency;
+              
+              if ($frequency < $min_frequency AND $min_frequency > 0 AND $min_frequency < $max_frequency)
+              {
+                  $min_frequency = $frequency;
+              }
+              
+              if ($frequency > $min_frequency AND $min_frequency < 0)
+              {
+                  // Frequency values not set therefore initialize
+                  $min_frequency = $frequency;
+                  $max_frequency = $min_frequency;
+              }
+              
+              // Set the maximum frequency
+              $max_frequency = ($frequency > $max_frequency AND $max_frequency > 0 AND $frequency > $min_frequency)
+                  ? $frequency
+                  : $max_frequency;
+          }
+                    
+          // Generate the tag cloud
+          $tag_cloud_html = '<div class="clearingfix"></div>';
+          $tag_cloud_html .= '<div style="width: 100%; height: auto; margin-top:30px; ">';
+          $tag_cloud_html .= '<h5>Category Cloud</h5>';
+          
+          foreach ($category_items as $item)
+          {
+              // Get the current frequency
+              $frequency = (int)$item->tag_frequency;
+              
+              // Get the font size of the cloud item
+              $font_size = ($this->_calculate_font_size($frequency, $max_frequency, $min_frequency));
+              
+              // Generate the html
+              $tag_cloud_html .= '<span style="padding:0px 10px;">';
+              $tag_cloud_html .= '<a href="'.url::base().'reports/index/c?='.(int)$item->id.'" style="font-size:'.$font_size.'px; font-weight:bold; color: #'.$item->category_color.'">'.$item->category_title.'</a>';
+              $tag_cloud_html .= '</span>';
+          }
+          
+          $tag_cloud_html .= '</div>';
+          
+          echo $tag_cloud_html;
+      }
+      
+      /**
+       * Calculates the font size of a tag cloud item
+       */
+      private function _calculate_font_size($cur_frequency, $max_frequency, $min_frequency)
+      {
+          // Calculate the weight using a power law - consider switching to a logarithmic one
+          $weight = ($cur_frequency - $min_frequency)/($max_frequency - $min_frequency);
+          
+          return $this->min_font_size + num::round(($this->max_font_size - $this->min_font_size) * $weight);
+      }
+ }
+ 
+ new categorycloud;
